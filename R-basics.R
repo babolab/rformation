2e23086a9aab13ba40c4bde7391d86a27e8971cc
babolab <geoffrey.babault@gmail.com>
2021-06-01 17:33:16 +0200

    2.4.3

---
+++ R-basics.R
@@ -0,0 +1,22 @@
+a=1
+b=1
+c <- -1
+(-b - sqrt(b^2 - 4*a*c) ) / ( 2*a )
+
+# Ceci est un essai
+a <- 3
+b <- 2
+c <- -1
+(-b + sqrt(b^2 - 4*a*c)) / (2*a)
+(-b - sqrt(b^2 - 4*a*c)) / (2*a)
+
+n<-1000
+x<- seq(1,n)
+r=sum(x)
+log(sqrt(100), base=10)
+
+library(dslabs)
+data(murders)
+class (murders)
+str(murders)
+head(murders)
